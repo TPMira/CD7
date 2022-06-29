4e9004dc4fc3d2ff67f8e2eabd069745c5dbcc30
thiagodepaulamira <thiagodepaulamira@gmail.com>
2022-06-29 19:56:48 -0300

    att dummy

--- cd7/src/data/dummy.jsx
+++ cd7/src/data/dummy.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FaHome } from 'react-icons/fa';
+import { FaHome, FaVectorSquare } from 'react-icons/fa';
 import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
 import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
 import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
 import { BiColorFill } from 'react-icons/bi';
-import { IoMdContacts } from 'react-icons/io';
+import { IoMdContacts, IoMdSwap } from 'react-icons/io';
 import { RiContactsLine, RiStockLine } from 'react-icons/ri';
 
 import { GiLouvrePyramid } from 'react-icons/gi';
@@ -21,72 +21,17 @@ export const links = [
     ],
   },
 
-  {
-    title: 'Pages',
-    links: [
-      {
-        name: 'login',
-        icon: <AiOutlineShoppingCart />,
-      },
-      
-    ],
-  },
+ 
   {
     title: 'blockchain',
     links: [
       {
         name: 'swap-wallet',
-        icon: <AiOutlineCalendar />,
+        icon: <IoMdSwap />,
       },
       {
         name: 'blocks',
-        icon: <BsKanban />,
-      },
-      {
-        name: 'editor',
-        icon: <FiEdit />,
-      },
-      {
-        name: 'color-picker',
-        icon: <BiColorFill />,
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    links: [
-      {
-        name: 'line',
-        icon: <AiOutlineStock />,
-      },
-      {
-        name: 'area',
-        icon: <AiOutlineAreaChart />,
-      },
-
-      {
-        name: 'bar',
-        icon: <AiOutlineBarChart />,
-      },
-      {
-        name: 'pie',
-        icon: <FiPieChart />,
-      },
-      {
-        name: 'financial',
-        icon: <RiStockLine />,
-      },
-      {
-        name: 'color-mapping',
-        icon: <BsBarChart />,
-      },
-      {
-        name: 'pyramid',
-        icon: <GiLouvrePyramid />,
-      },
-      {
-        name: 'stacked',
-        icon: <AiOutlineBarChart />,
+        icon: <FaVectorSquare />,
       },
     ],
   },
